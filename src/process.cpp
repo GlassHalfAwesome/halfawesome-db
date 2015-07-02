3217ca90fa3d4e2a157ac88f53bfad1fb8cd565a
GlassHalfAwesome <glass@glasshalfawesome.com>
2015-07-02 01:23:07 -0300

    Restructured a bit with some namespaces

--- src/process.cpp
+++ src/process.cpp
@@ -5,90 +5,106 @@
 #include <vector>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <syslog.h>
 
-void error(const std::string& error) {
-	std::ofstream file;
-
-	file.open("logs/errors", std::ios_base::app); // I should error check this
-	file << error << '\n';
-	file.close();
-}
-
-bool exists(const std::string& filename) {
-	struct stat buffer;   
-
-	return stat(filename.data(), &buffer) == 0; 
-}
-
-std::vector<std::string> parser(const std::string& query) {
-	std::vector<std::string> elems;
-	std::stringstream ss {query};
-	std::string s;
-
-	while (std::getline(ss, s, ' ')) {
-		elems.push_back(s);
-	}
-
-	return elems;
-}
-
-void create(const std::vector<std::string>& parsed) {
-	if (exists(parsed[1])) {
-		error("Create failed: file already exists.");
-	} else {
+// Log functions
+namespace log {
+	void error(const std::string& error) {
 		std::ofstream file;
 
-		file.open(parsed[1]); // I should error check this
-		file << "Creating.\n";
+		file.open("logs/errors", std::ios_base::app); // I should error check this
+		file << error << '\n';
 		file.close();
 	}
 
-}
-
-void select(const std::vector<std::string>& parsed) {
-	// Do work
-}
-
-void update(const std::vector<std::string>& parsed) {
-	if (exists(parsed[1])) {
+	void query(const std::string& query) {
 		std::ofstream file;
 
-		file.open(parsed[1], std::ios_base::app); // I should error check this
-		file << "Appending.\n";
+		file.open("logs/queries", std::ios_base::app); // I should error check this
+		file << query << '\n';
 		file.close();
-	} else {
-		error("Update failed: file doesn't exist.");
 	}
 }
 
-void del(const std::vector<std::string>& parsed) {
-	if (exists(parsed[1])) {
-		std::remove(parsed[1].data()); // I should error check this
-	} else {
-		error("Delete failed: file doesn't exist.");
+// Utility functions
+namespace util {
+	bool exists(const std::string& filename) {
+		struct stat buffer;   
+
+		return stat(filename.data(), &buffer) == 0; 
+	}
+
+	std::vector<std::string> parser(const std::string& query) {
+		std::vector<std::string> elems;
+		std::stringstream ss {query};
+		std::string s;
+
+		while (std::getline(ss, s, ' ')) {
+			elems.push_back(s);
+		}
+
+		return elems;
+	}
+}
+
+// Keyword functions
+namespace kw {
+	void create(const std::vector<std::string>& parsed) {
+		if (util::exists(parsed[1])) {
+			log::error("Create failed: file already exists.");
+		} else {
+			std::ofstream file;
+
+			file.open(parsed[1]); // I should error check this
+			file << "Creating.\n";
+			file.close();
+		}
+	}
+
+	void select(const std::vector<std::string>& parsed) {
+		// Do work
+	}
+
+	void update(const std::vector<std::string>& parsed) {
+		if (util::exists(parsed[1])) {
+			std::ofstream file;
+
+			file.open(parsed[1], std::ios_base::app); // I should error check this
+			file << "Appending.\n";
+			file.close();
+		} else {
+			log::error("Update failed: file doesn't exist.");
+		}
+	}
+
+	void del(const std::vector<std::string>& parsed) {
+		if (util::exists(parsed[1])) {
+			std::remove(parsed[1].data()); // I should error check this
+		} else {
+			log::error("Delete failed: file doesn't exist.");
+		}
 	}
 }
 
 void interpreter(const std::string& query) {
-	std::vector<std::string> parsed {parser(query)};
+	std::vector<std::string> parsed {util::parser(query)};
 	if (parsed.size() > 1) {
 		if (parsed[0] == "CREATE") {
-			create(parsed);
+			kw::create(parsed);
 		} else if (parsed[0] == "SELECT") {
-			select(parsed);
+			kw::select(parsed);
 		} else if (parsed[0] == "UPDATE") {
-			update(parsed);
+			kw::update(parsed);
 		} else if (parsed[0] == "DELETE") {
-			del(parsed);
+			kw::del(parsed);
 		} else {
-			error("Query failed: keyword doesn't exist.");
+			log::error("Query failed: keyword doesn't exist.");
 		}
 	} else {
-		error("Query failed: filename not specified.");
+		log::error("Query failed: filename not specified.");
 	}
 }
 
 void process(const std::string& query) {
+	log::query(query);
 	interpreter(query);
 }
